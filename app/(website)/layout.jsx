99341c50fcc03b83d8e7644cc04cc8dc63d4d89e
sameershaikh <sameer@prosperaahospitality.com>
2024-10-21 13:38:51 +0530

    facebook meta tag

--- app/(website)/layout.jsx
+++ app/(website)/layout.jsx
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import '@/app/styles/globals.css';
+import '@/app/styles/globals.css'
 import { Providers } from "../providers";
 import SiteNavbar from "@/_components/SiteNavbar";
 import SiteFooter from "@/_components/footer/SiteFooter";
@@ -9,17 +9,14 @@ export const metadata = {
   title: "Vinifera",
   description: "Vinifera",
   icons: "/Vinifera.svg",
+  other: {
+    "facebook-domain-verification": "0cr9z5v0grizm39hs7fftxf0qvw66i"
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta name="facebook-domain-verification" content="0cr9z5v0grizm39hs7fftxf0qvw66i" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href={metadata.icons} />
-      </head>
       <body>
         <Providers>
           <ClientLayout>

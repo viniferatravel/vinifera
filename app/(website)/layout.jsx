cba4735bbd4cf66d7b4acbe7a7018c76398c63aa
sameershaikh <sameer@prosperaahospitality.com>
2024-10-21 13:21:36 +0530

    facebook verification

--- app/(website)/layout.jsx
+++ app/(website)/layout.jsx
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import '@/app/styles/globals.css'
+import '@/app/styles/globals.css';
 import { Providers } from "../providers";
 import SiteNavbar from "@/_components/SiteNavbar";
 import SiteFooter from "@/_components/footer/SiteFooter";
@@ -8,12 +8,18 @@ import ClientLayout from "@/_components/Home/ClientLayout";
 export const metadata = {
   title: "Vinifera",
   description: "Vinifera",
-  icons: "/Vinifera.svg"
+  icons: "/Vinifera.svg",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="facebook-domain-verification" content="0cr9z5v0grizm39hs7fftxf0qvw66i" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons} />
+      </head>
       <body>
         <Providers>
           <ClientLayout>
